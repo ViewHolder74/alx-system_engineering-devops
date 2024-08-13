c8a0466db73d48168c9d429fd125fac28ad2f3fa
Mussa Milanzi <viewholder74@gmail.com>
2024-08-13 22:39:24 +0000

    Update 1-top_ten function

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-"""
-1-top_ten.py
-"""
+"""Script that returns top 10 hot posts of a subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-	"""
-	Queries the Reddit API and prints the titles of the first 10 hot posts
-	listed for a given subreddit.
-	"""
-	url = 'https://www.reddit.com/r/{}/hot.json?show="all"&limit=10'.format(
-        subreddit)
-	headers = {'User-Agent': 'Custome-Agent'}
-	response = requests.get(url, headers=headers)
-	try:
-		top_ten = response.json()['data']['children']
-		for post in top_ten:
-			print(post['data']['title'])
-	except KeyError:
-		print("None")
+    """Function that prints top 10 posts of a subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
+        print(None)
+        return
+
+    headers = {'User-Agent': 'selBot/2.0'}
+    URL = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+
+    try:
+        response = requests.get(URL, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json()
+        posts = data['data']['children']
+
+        if not posts:
+            print("None")
+
+        else:
+            for post in posts:
+                title = post['data']['title']
+                print(title)
+
+    except requests.exceptions.RequestException:
+        print(None)
+
+    except (KeyError, ValueError):
+        print(None)
