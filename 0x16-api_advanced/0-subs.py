86ef35e82e138ee4662d86a0bd64407253c4780d
Mussa Milanzi <viewholder74@gmail.com>
2024-08-13 23:02:57 +0000

    Task 0 Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""
-0-subs.py
-"""
+"""Script that returns no. of subscribers of a subreddit"""
 import requests
 
-def number_of_subscribers(subreddit):
-	"""
-	The function querries the Reddit API and
-	return the number of subscribers(mot active users, total subscribers)
-	dor a give subreddit.
-	"""
-	url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-	headers = {"User-Agent": "custom_user_agent"}
 
-	response = requests.get(url, headers=headers)
-	if (not response.ok):
+def number_of_subscribers(subreddit):
+	"""Function that returns no. of subs of a subreddit"""
+	if subreddit is None or not isinstance(subreddit, str):
 		return 0
-	sub_count = response.json().get('data').get('subscribers')
-	return sub_count	
+	headers = {'User-Agent': 'selBot/1.0'}
+	URL = f'https://www.reddit.com/r/{subreddit}/about.json'
+	try:
+		response = requests.get(URL, headers=headers, allow_redirects=False)
+		response.raise_for_status()
+		data = response.json()
+		subscribers = data['data']['subscribers']
+		return subscribers
+	except requests.exceptions.RequestException:
+		return 0
+	except (KeyError, ValueError):
+		return 0
